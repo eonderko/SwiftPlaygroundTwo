9f79f00f2e81dd36038617016069e6997f3e4885
eonderko <eonderko@hawaii.edu>
2020-02-04 09:52:58 -1000

    initial commit

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
